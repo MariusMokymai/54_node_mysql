78754d46e826d6787181afa8974113585ba96a06
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-02 14:43:52 +0200

    front cats

---
+++ front/front.js
@@ -0,0 +1,12 @@
+'use strict';
+console.log('front.js file was loaded');
+
+const catUrl = 'http://localhost:3000/api/categories';
+
+// parsisiusti kategorijas ir iskonsolinti
+
+function getCategories() {
+  // parsiusti, iskonsolinti ir grazinti categorijas
+}
+
+// sugeneruoti opcijas selectui
