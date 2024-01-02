54977c982f38e039f09693b0dc73a2276b9927be
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-02 14:34:02 +0200

    all cats

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const { getSqlData } = require('../helper');
 
 const categoriesRouter = express.Router();
 
 // routes
-// GET /api.categories - visas kategorijas
+// GET /api/categories - visas kategorijas
+
+categoriesRouter.get('/api/categories', async (req, res) => {
+  const sql = 'SELECT * FROM `categories`';
+
+  const [categoriesArr, error] = await getSqlData(sql);
+
+  if (error) {
+    // euston we have a proplem
+    console.log('error ===', error);
+    res.status(500).json('something wrong');
+    return;
+  }
+
+  res.json(categoriesArr);
+});
 
 module.exports = categoriesRouter;
