f9bc8e6e75dd674047ddf4cefcbdd7a51bfb9128
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-15 13:32:46 +0200

    posts.getAll

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -5,29 +5,13 @@ const { getSqlData } = require('../helper');
 const { dbConfig } = require('../config');
 const { authorizeToken, validatePostBody } = require('../middleware');
 
+const postsController = require('../controllers/postsController');
+
 const postsRouter = express.Router();
 
 // GET /api/posts - get all posts
 // SELECT * FROM `posts`
-postsRouter.get('/api/posts', authorizeToken, async (req, res, next) => {
-  // const sql = 'SELECT * FROM posts';
-  const sql = `
-  SELECT posts.post_id, posts.title, posts.author, posts.content, posts.date, COUNT(post_comments.comm_id) AS commentCount,
-  categories.title AS categoryName
-  FROM posts
-  JOIN categories
-  ON posts.cat_id=categories.cat_id
-  LEFT JOIN post_comments
-  ON post_comments.post_id=posts.post_id
-  GROUP BY posts.post_id
-  `;
-
-  const [postsArr, error] = await getSqlData(sql);
-
-  if (error) return next(error);
-
-  res.json(postsArr);
-});
+postsRouter.get('/api/posts', authorizeToken, postsController.getAll);
 
 // GET /api/posts/2 - get post su id 2
 postsRouter.get(
